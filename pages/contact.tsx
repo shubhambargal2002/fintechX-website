f4acdf33f177d32ce80262a053d09cd1f41ca71a
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-05-22 18:57:13 +0530

    contact page created

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,6 @@
 import { CanvasZone } from "@atrilabs/canvas-zone";
 import "../styles/common.css";
+import "../styles/contact.css";
 
 export default function() {
 	return <CanvasZone id={"contact"} />
