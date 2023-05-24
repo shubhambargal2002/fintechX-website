26d9ea6377201ea8c9df6f8f0db11f1576583c53
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-05-24 17:07:38 +0530

    forgot_password page created

--- controllers/routes/forgot_password_model.py
+++ controllers/routes/forgot_password_model.py
@@ -1,5 +1,10 @@
 from typing import Union, Any
-
+from atri_react.Flex import Flex
+from manifests.FramerFlex import FramerFlex
+from atri_react.Image import Image
+from atri_react.TextBox import TextBox
+from atri_react.Input import Input
+from manifests.ButtonFlex import ButtonFlex
 
 
   
@@ -8,7 +13,17 @@ class Page:
 		self.event_data = None
 		self.event_alias = None
 		self._setter_access_tracker = {}
-
+		self.Flex1 = state["Flex1"] if "Flex1" in state else None
+		self.Flex2 = state["Flex2"] if "Flex2" in state else None
+		self.FramerFlex1 = state["FramerFlex1"] if "FramerFlex1" in state else None
+		self.Image1 = state["Image1"] if "Image1" in state else None
+		self.Flex3 = state["Flex3"] if "Flex3" in state else None
+		self.Flex4 = state["Flex4"] if "Flex4" in state else None
+		self.TextBox1 = state["TextBox1"] if "TextBox1" in state else None
+		self.TextBox2 = state["TextBox2"] if "TextBox2" in state else None
+		self.Input1 = state["Input1"] if "Input1" in state else None
+		self.ButtonFlex1 = state["ButtonFlex1"] if "ButtonFlex1" in state else None
+		self.TextBox3 = state["TextBox3"] if "TextBox3" in state else None
 		self._setter_access_tracker = {}
 		self._getter_access_tracker = {}
   
@@ -22,9 +37,117 @@ class Page:
 		self.event_repeating = event["repeating"]
   
 	
+	@property
+	def Flex1(self):
+		self._getter_access_tracker["Flex1"] = {}
+		return self._Flex1
+	@Flex1.setter
+	def Flex1(self, new_state):
+		self._setter_access_tracker["Flex1"] = {}
+		self._Flex1 = Flex(new_state)
+
+	@property
+	def Flex2(self):
+		self._getter_access_tracker["Flex2"] = {}
+		return self._Flex2
+	@Flex2.setter
+	def Flex2(self, new_state):
+		self._setter_access_tracker["Flex2"] = {}
+		self._Flex2 = Flex(new_state)
+
+	@property
+	def FramerFlex1(self):
+		self._getter_access_tracker["FramerFlex1"] = {}
+		return self._FramerFlex1
+	@FramerFlex1.setter
+	def FramerFlex1(self, new_state):
+		self._setter_access_tracker["FramerFlex1"] = {}
+		self._FramerFlex1 = FramerFlex(new_state)
+
+	@property
+	def Image1(self):
+		self._getter_access_tracker["Image1"] = {}
+		return self._Image1
+	@Image1.setter
+	def Image1(self, new_state):
+		self._setter_access_tracker["Image1"] = {}
+		self._Image1 = Image(new_state)
+
+	@property
+	def Flex3(self):
+		self._getter_access_tracker["Flex3"] = {}
+		return self._Flex3
+	@Flex3.setter
+	def Flex3(self, new_state):
+		self._setter_access_tracker["Flex3"] = {}
+		self._Flex3 = Flex(new_state)
+
+	@property
+	def Flex4(self):
+		self._getter_access_tracker["Flex4"] = {}
+		return self._Flex4
+	@Flex4.setter
+	def Flex4(self, new_state):
+		self._setter_access_tracker["Flex4"] = {}
+		self._Flex4 = Flex(new_state)
+
+	@property
+	def TextBox1(self):
+		self._getter_access_tracker["TextBox1"] = {}
+		return self._TextBox1
+	@TextBox1.setter
+	def TextBox1(self, new_state):
+		self._setter_access_tracker["TextBox1"] = {}
+		self._TextBox1 = TextBox(new_state)
+
+	@property
+	def TextBox2(self):
+		self._getter_access_tracker["TextBox2"] = {}
+		return self._TextBox2
+	@TextBox2.setter
+	def TextBox2(self, new_state):
+		self._setter_access_tracker["TextBox2"] = {}
+		self._TextBox2 = TextBox(new_state)
+
+	@property
+	def Input1(self):
+		self._getter_access_tracker["Input1"] = {}
+		return self._Input1
+	@Input1.setter
+	def Input1(self, new_state):
+		self._setter_access_tracker["Input1"] = {}
+		self._Input1 = Input(new_state)
+
+	@property
+	def ButtonFlex1(self):
+		self._getter_access_tracker["ButtonFlex1"] = {}
+		return self._ButtonFlex1
+	@ButtonFlex1.setter
+	def ButtonFlex1(self, new_state):
+		self._setter_access_tracker["ButtonFlex1"] = {}
+		self._ButtonFlex1 = ButtonFlex(new_state)
+
+	@property
+	def TextBox3(self):
+		self._getter_access_tracker["TextBox3"] = {}
+		return self._TextBox3
+	@TextBox3.setter
+	def TextBox3(self, new_state):
+		self._setter_access_tracker["TextBox3"] = {}
+		self._TextBox3 = TextBox(new_state)
   
 	def _to_json_fields(self):
 		return {
-
+			"Flex1": self._Flex1,
+			"Flex2": self._Flex2,
+			"FramerFlex1": self._FramerFlex1,
+			"Image1": self._Image1,
+			"Flex3": self._Flex3,
+			"Flex4": self._Flex4,
+			"TextBox1": self._TextBox1,
+			"TextBox2": self._TextBox2,
+			"Input1": self._Input1,
+			"ButtonFlex1": self._ButtonFlex1,
+			"TextBox3": self._TextBox3
 			}
   
