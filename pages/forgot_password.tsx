6927957f4e11b7e0480c6e38a6a7bef67f53f1b8
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-05-24 16:42:28 +0530

    coming_soon page created

--- pages/forgot_password.tsx
+++ pages/forgot_password.tsx
@@ -1,7 +1,6 @@
 import { CanvasZone } from "@atrilabs/canvas-zone";
 import "../styles/common.css";
-import "../styles/coming_soon.css";
 
 export default function() {
-	return <CanvasZone id={"coming_soon"} />
+	return <CanvasZone id={"forgot_password"} />
 }
