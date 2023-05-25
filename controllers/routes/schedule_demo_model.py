d27d018513be5ba038556242a7ebb61ec84a1035
shubhambargal2002 <shubhambargal2002@gmail.com>
2023-05-25 19:44:35 +0530

    schedule demo page created

--- controllers/routes/schedule_demo_model.py
+++ controllers/routes/schedule_demo_model.py
@@ -1,5 +1,11 @@
 from typing import Union, Any
-
+from atri_react.Flex import Flex
+from manifests.FramerFlex import FramerFlex
+from atri_react.TextBox import TextBox
+from atri_react.Image import Image
+from atri_react.Input import Input
+from atri_react.Textarea import Textarea
+from manifests.ButtonFlex import ButtonFlex
 
 
   
@@ -8,7 +14,56 @@ class Page:
 		self.event_data = None
 		self.event_alias = None
 		self._setter_access_tracker = {}
-
+		self.Flex1 = state["Flex1"] if "Flex1" in state else None
+		self.Flex2 = state["Flex2"] if "Flex2" in state else None
+		self.Flex3 = state["Flex3"] if "Flex3" in state else None
+		self.Flex4 = state["Flex4"] if "Flex4" in state else None
+		self.FramerFlex1 = state["FramerFlex1"] if "FramerFlex1" in state else None
+		self.FramerFlex2 = state["FramerFlex2"] if "FramerFlex2" in state else None
+		self.Flex5 = state["Flex5"] if "Flex5" in state else None
+		self.Flex7 = state["Flex7"] if "Flex7" in state else None
+		self.TextBox1 = state["TextBox1"] if "TextBox1" in state else None
+		self.TextBox2 = state["TextBox2"] if "TextBox2" in state else None
+		self.Flex8 = state["Flex8"] if "Flex8" in state else None
+		self.Flex9 = state["Flex9"] if "Flex9" in state else None
+		self.Image1 = state["Image1"] if "Image1" in state else None
+		self.Flex10 = state["Flex10"] if "Flex10" in state else None
+		self.TextBox3 = state["TextBox3"] if "TextBox3" in state else None
+		self.TextBox4 = state["TextBox4"] if "TextBox4" in state else None
+		self.Flex11 = state["Flex11"] if "Flex11" in state else None
+		self.TextBox5 = state["TextBox5"] if "TextBox5" in state else None
+		self.Flex12 = state["Flex12"] if "Flex12" in state else None
+		self.Flex13 = state["Flex13"] if "Flex13" in state else None
+		self.TextBox6 = state["TextBox6"] if "TextBox6" in state else None
+		self.Image2 = state["Image2"] if "Image2" in state else None
+		self.Image3 = state["Image3"] if "Image3" in state else None
+		self.TextBox7 = state["TextBox7"] if "TextBox7" in state else None
+		self.Flex14 = state["Flex14"] if "Flex14" in state else None
+		self.Flex15 = state["Flex15"] if "Flex15" in state else None
+		self.Image4 = state["Image4"] if "Image4" in state else None
+		self.TextBox8 = state["TextBox8"] if "TextBox8" in state else None
+		self.Flex16 = state["Flex16"] if "Flex16" in state else None
+		self.Flex17 = state["Flex17"] if "Flex17" in state else None
+		self.Flex18 = state["Flex18"] if "Flex18" in state else None
+		self.Flex19 = state["Flex19"] if "Flex19" in state else None
+		self.Input2 = state["Input2"] if "Input2" in state else None
+		self.Input3 = state["Input3"] if "Input3" in state else None
+		self.Flex20 = state["Flex20"] if "Flex20" in state else None
+		self.Input4 = state["Input4"] if "Input4" in state else None
+		self.Input5 = state["Input5"] if "Input5" in state else None
+		self.Flex21 = state["Flex21"] if "Flex21" in state else None
+		self.Flex22 = state["Flex22"] if "Flex22" in state else None
+		self.Flex23 = state["Flex23"] if "Flex23" in state else None
+		self.Input6 = state["Input6"] if "Input6" in state else None
+		self.Input7 = state["Input7"] if "Input7" in state else None
+		self.Flex24 = state["Flex24"] if "Flex24" in state else None
+		self.Flex25 = state["Flex25"] if "Flex25" in state else None
+		self.Flex26 = state["Flex26"] if "Flex26" in state else None
+		self.Flex27 = state["Flex27"] if "Flex27" in state else None
+		self.Textarea1 = state["Textarea1"] if "Textarea1" in state else None
+		self.ButtonFlex1 = state["ButtonFlex1"] if "ButtonFlex1" in state else None
+		self.TextBox9 = state["TextBox9"] if "TextBox9" in state else None
+		self.Flex28 = state["Flex28"] if "Flex28" in state else None
 		self._setter_access_tracker = {}
 		self._getter_access_tracker = {}
   
@@ -22,9 +77,507 @@ class Page:
 		self.event_repeating = event["repeating"]
   
 	
+	@property
+	def Flex1(self):
+		self._getter_access_tracker["Flex1"] = {}
+		return self._Flex1
+	@Flex1.setter
+	def Flex1(self, new_state):
+		self._setter_access_tracker["Flex1"] = {}
+		self._Flex1 = Flex(new_state)
+
+	@property
+	def Flex2(self):
+		self._getter_access_tracker["Flex2"] = {}
+		return self._Flex2
+	@Flex2.setter
+	def Flex2(self, new_state):
+		self._setter_access_tracker["Flex2"] = {}
+		self._Flex2 = Flex(new_state)
+
+	@property
+	def Flex3(self):
+		self._getter_access_tracker["Flex3"] = {}
+		return self._Flex3
+	@Flex3.setter
+	def Flex3(self, new_state):
+		self._setter_access_tracker["Flex3"] = {}
+		self._Flex3 = Flex(new_state)
+
+	@property
+	def Flex4(self):
+		self._getter_access_tracker["Flex4"] = {}
+		return self._Flex4
+	@Flex4.setter
+	def Flex4(self, new_state):
+		self._setter_access_tracker["Flex4"] = {}
+		self._Flex4 = Flex(new_state)
+
+	@property
+	def FramerFlex1(self):
+		self._getter_access_tracker["FramerFlex1"] = {}
+		return self._FramerFlex1
+	@FramerFlex1.setter
+	def FramerFlex1(self, new_state):
+		self._setter_access_tracker["FramerFlex1"] = {}
+		self._FramerFlex1 = FramerFlex(new_state)
+
+	@property
+	def FramerFlex2(self):
+		self._getter_access_tracker["FramerFlex2"] = {}
+		return self._FramerFlex2
+	@FramerFlex2.setter
+	def FramerFlex2(self, new_state):
+		self._setter_access_tracker["FramerFlex2"] = {}
+		self._FramerFlex2 = FramerFlex(new_state)
+
+	@property
+	def Flex5(self):
+		self._getter_access_tracker["Flex5"] = {}
+		return self._Flex5
+	@Flex5.setter
+	def Flex5(self, new_state):
+		self._setter_access_tracker["Flex5"] = {}
+		self._Flex5 = Flex(new_state)
+
+	@property
+	def Flex7(self):
+		self._getter_access_tracker["Flex7"] = {}
+		return self._Flex7
+	@Flex7.setter
+	def Flex7(self, new_state):
+		self._setter_access_tracker["Flex7"] = {}
+		self._Flex7 = Flex(new_state)
+
+	@property
+	def TextBox1(self):
+		self._getter_access_tracker["TextBox1"] = {}
+		return self._TextBox1
+	@TextBox1.setter
+	def TextBox1(self, new_state):
+		self._setter_access_tracker["TextBox1"] = {}
+		self._TextBox1 = TextBox(new_state)
+
+	@property
+	def TextBox2(self):
+		self._getter_access_tracker["TextBox2"] = {}
+		return self._TextBox2
+	@TextBox2.setter
+	def TextBox2(self, new_state):
+		self._setter_access_tracker["TextBox2"] = {}
+		self._TextBox2 = TextBox(new_state)
+
+	@property
+	def Flex8(self):
+		self._getter_access_tracker["Flex8"] = {}
+		return self._Flex8
+	@Flex8.setter
+	def Flex8(self, new_state):
+		self._setter_access_tracker["Flex8"] = {}
+		self._Flex8 = Flex(new_state)
+
+	@property
+	def Flex9(self):
+		self._getter_access_tracker["Flex9"] = {}
+		return self._Flex9
+	@Flex9.setter
+	def Flex9(self, new_state):
+		self._setter_access_tracker["Flex9"] = {}
+		self._Flex9 = Flex(new_state)
+
+	@property
+	def Image1(self):
+		self._getter_access_tracker["Image1"] = {}
+		return self._Image1
+	@Image1.setter
+	def Image1(self, new_state):
+		self._setter_access_tracker["Image1"] = {}
+		self._Image1 = Image(new_state)
+
+	@property
+	def Flex10(self):
+		self._getter_access_tracker["Flex10"] = {}
+		return self._Flex10
+	@Flex10.setter
+	def Flex10(self, new_state):
+		self._setter_access_tracker["Flex10"] = {}
+		self._Flex10 = Flex(new_state)
+
+	@property
+	def TextBox3(self):
+		self._getter_access_tracker["TextBox3"] = {}
+		return self._TextBox3
+	@TextBox3.setter
+	def TextBox3(self, new_state):
+		self._setter_access_tracker["TextBox3"] = {}
+		self._TextBox3 = TextBox(new_state)
+
+	@property
+	def TextBox4(self):
+		self._getter_access_tracker["TextBox4"] = {}
+		return self._TextBox4
+	@TextBox4.setter
+	def TextBox4(self, new_state):
+		self._setter_access_tracker["TextBox4"] = {}
+		self._TextBox4 = TextBox(new_state)
+
+	@property
+	def Flex11(self):
+		self._getter_access_tracker["Flex11"] = {}
+		return self._Flex11
+	@Flex11.setter
+	def Flex11(self, new_state):
+		self._setter_access_tracker["Flex11"] = {}
+		self._Flex11 = Flex(new_state)
+
+	@property
+	def TextBox5(self):
+		self._getter_access_tracker["TextBox5"] = {}
+		return self._TextBox5
+	@TextBox5.setter
+	def TextBox5(self, new_state):
+		self._setter_access_tracker["TextBox5"] = {}
+		self._TextBox5 = TextBox(new_state)
+
+	@property
+	def Flex12(self):
+		self._getter_access_tracker["Flex12"] = {}
+		return self._Flex12
+	@Flex12.setter
+	def Flex12(self, new_state):
+		self._setter_access_tracker["Flex12"] = {}
+		self._Flex12 = Flex(new_state)
+
+	@property
+	def Flex13(self):
+		self._getter_access_tracker["Flex13"] = {}
+		return self._Flex13
+	@Flex13.setter
+	def Flex13(self, new_state):
+		self._setter_access_tracker["Flex13"] = {}
+		self._Flex13 = Flex(new_state)
+
+	@property
+	def TextBox6(self):
+		self._getter_access_tracker["TextBox6"] = {}
+		return self._TextBox6
+	@TextBox6.setter
+	def TextBox6(self, new_state):
+		self._setter_access_tracker["TextBox6"] = {}
+		self._TextBox6 = TextBox(new_state)
+
+	@property
+	def Image2(self):
+		self._getter_access_tracker["Image2"] = {}
+		return self._Image2
+	@Image2.setter
+	def Image2(self, new_state):
+		self._setter_access_tracker["Image2"] = {}
+		self._Image2 = Image(new_state)
+
+	@property
+	def Image3(self):
+		self._getter_access_tracker["Image3"] = {}
+		return self._Image3
+	@Image3.setter
+	def Image3(self, new_state):
+		self._setter_access_tracker["Image3"] = {}
+		self._Image3 = Image(new_state)
+
+	@property
+	def TextBox7(self):
+		self._getter_access_tracker["TextBox7"] = {}
+		return self._TextBox7
+	@TextBox7.setter
+	def TextBox7(self, new_state):
+		self._setter_access_tracker["TextBox7"] = {}
+		self._TextBox7 = TextBox(new_state)
+
+	@property
+	def Flex14(self):
+		self._getter_access_tracker["Flex14"] = {}
+		return self._Flex14
+	@Flex14.setter
+	def Flex14(self, new_state):
+		self._setter_access_tracker["Flex14"] = {}
+		self._Flex14 = Flex(new_state)
+
+	@property
+	def Flex15(self):
+		self._getter_access_tracker["Flex15"] = {}
+		return self._Flex15
+	@Flex15.setter
+	def Flex15(self, new_state):
+		self._setter_access_tracker["Flex15"] = {}
+		self._Flex15 = Flex(new_state)
+
+	@property
+	def Image4(self):
+		self._getter_access_tracker["Image4"] = {}
+		return self._Image4
+	@Image4.setter
+	def Image4(self, new_state):
+		self._setter_access_tracker["Image4"] = {}
+		self._Image4 = Image(new_state)
+
+	@property
+	def TextBox8(self):
+		self._getter_access_tracker["TextBox8"] = {}
+		return self._TextBox8
+	@TextBox8.setter
+	def TextBox8(self, new_state):
+		self._setter_access_tracker["TextBox8"] = {}
+		self._TextBox8 = TextBox(new_state)
+
+	@property
+	def Flex16(self):
+		self._getter_access_tracker["Flex16"] = {}
+		return self._Flex16
+	@Flex16.setter
+	def Flex16(self, new_state):
+		self._setter_access_tracker["Flex16"] = {}
+		self._Flex16 = Flex(new_state)
+
+	@property
+	def Flex17(self):
+		self._getter_access_tracker["Flex17"] = {}
+		return self._Flex17
+	@Flex17.setter
+	def Flex17(self, new_state):
+		self._setter_access_tracker["Flex17"] = {}
+		self._Flex17 = Flex(new_state)
+
+	@property
+	def Flex18(self):
+		self._getter_access_tracker["Flex18"] = {}
+		return self._Flex18
+	@Flex18.setter
+	def Flex18(self, new_state):
+		self._setter_access_tracker["Flex18"] = {}
+		self._Flex18 = Flex(new_state)
+
+	@property
+	def Flex19(self):
+		self._getter_access_tracker["Flex19"] = {}
+		return self._Flex19
+	@Flex19.setter
+	def Flex19(self, new_state):
+		self._setter_access_tracker["Flex19"] = {}
+		self._Flex19 = Flex(new_state)
+
+	@property
+	def Input2(self):
+		self._getter_access_tracker["Input2"] = {}
+		return self._Input2
+	@Input2.setter
+	def Input2(self, new_state):
+		self._setter_access_tracker["Input2"] = {}
+		self._Input2 = Input(new_state)
+
+	@property
+	def Input3(self):
+		self._getter_access_tracker["Input3"] = {}
+		return self._Input3
+	@Input3.setter
+	def Input3(self, new_state):
+		self._setter_access_tracker["Input3"] = {}
+		self._Input3 = Input(new_state)
+
+	@property
+	def Flex20(self):
+		self._getter_access_tracker["Flex20"] = {}
+		return self._Flex20
+	@Flex20.setter
+	def Flex20(self, new_state):
+		self._setter_access_tracker["Flex20"] = {}
+		self._Flex20 = Flex(new_state)
+
+	@property
+	def Input4(self):
+		self._getter_access_tracker["Input4"] = {}
+		return self._Input4
+	@Input4.setter
+	def Input4(self, new_state):
+		self._setter_access_tracker["Input4"] = {}
+		self._Input4 = Input(new_state)
+
+	@property
+	def Input5(self):
+		self._getter_access_tracker["Input5"] = {}
+		return self._Input5
+	@Input5.setter
+	def Input5(self, new_state):
+		self._setter_access_tracker["Input5"] = {}
+		self._Input5 = Input(new_state)
+
+	@property
+	def Flex21(self):
+		self._getter_access_tracker["Flex21"] = {}
+		return self._Flex21
+	@Flex21.setter
+	def Flex21(self, new_state):
+		self._setter_access_tracker["Flex21"] = {}
+		self._Flex21 = Flex(new_state)
+
+	@property
+	def Flex22(self):
+		self._getter_access_tracker["Flex22"] = {}
+		return self._Flex22
+	@Flex22.setter
+	def Flex22(self, new_state):
+		self._setter_access_tracker["Flex22"] = {}
+		self._Flex22 = Flex(new_state)
+
+	@property
+	def Flex23(self):
+		self._getter_access_tracker["Flex23"] = {}
+		return self._Flex23
+	@Flex23.setter
+	def Flex23(self, new_state):
+		self._setter_access_tracker["Flex23"] = {}
+		self._Flex23 = Flex(new_state)
+
+	@property
+	def Input6(self):
+		self._getter_access_tracker["Input6"] = {}
+		return self._Input6
+	@Input6.setter
+	def Input6(self, new_state):
+		self._setter_access_tracker["Input6"] = {}
+		self._Input6 = Input(new_state)
+
+	@property
+	def Input7(self):
+		self._getter_access_tracker["Input7"] = {}
+		return self._Input7
+	@Input7.setter
+	def Input7(self, new_state):
+		self._setter_access_tracker["Input7"] = {}
+		self._Input7 = Input(new_state)
+
+	@property
+	def Flex24(self):
+		self._getter_access_tracker["Flex24"] = {}
+		return self._Flex24
+	@Flex24.setter
+	def Flex24(self, new_state):
+		self._setter_access_tracker["Flex24"] = {}
+		self._Flex24 = Flex(new_state)
+
+	@property
+	def Flex25(self):
+		self._getter_access_tracker["Flex25"] = {}
+		return self._Flex25
+	@Flex25.setter
+	def Flex25(self, new_state):
+		self._setter_access_tracker["Flex25"] = {}
+		self._Flex25 = Flex(new_state)
+
+	@property
+	def Flex26(self):
+		self._getter_access_tracker["Flex26"] = {}
+		return self._Flex26
+	@Flex26.setter
+	def Flex26(self, new_state):
+		self._setter_access_tracker["Flex26"] = {}
+		self._Flex26 = Flex(new_state)
+
+	@property
+	def Flex27(self):
+		self._getter_access_tracker["Flex27"] = {}
+		return self._Flex27
+	@Flex27.setter
+	def Flex27(self, new_state):
+		self._setter_access_tracker["Flex27"] = {}
+		self._Flex27 = Flex(new_state)
+
+	@property
+	def Textarea1(self):
+		self._getter_access_tracker["Textarea1"] = {}
+		return self._Textarea1
+	@Textarea1.setter
+	def Textarea1(self, new_state):
+		self._setter_access_tracker["Textarea1"] = {}
+		self._Textarea1 = Textarea(new_state)
+
+	@property
+	def ButtonFlex1(self):
+		self._getter_access_tracker["ButtonFlex1"] = {}
+		return self._ButtonFlex1
+	@ButtonFlex1.setter
+	def ButtonFlex1(self, new_state):
+		self._setter_access_tracker["ButtonFlex1"] = {}
+		self._ButtonFlex1 = ButtonFlex(new_state)
+
+	@property
+	def TextBox9(self):
+		self._getter_access_tracker["TextBox9"] = {}
+		return self._TextBox9
+	@TextBox9.setter
+	def TextBox9(self, new_state):
+		self._setter_access_tracker["TextBox9"] = {}
+		self._TextBox9 = TextBox(new_state)
+
+	@property
+	def Flex28(self):
+		self._getter_access_tracker["Flex28"] = {}
+		return self._Flex28
+	@Flex28.setter
+	def Flex28(self, new_state):
+		self._setter_access_tracker["Flex28"] = {}
+		self._Flex28 = Flex(new_state)
   
 	def _to_json_fields(self):
 		return {
-
+			"Flex1": self._Flex1,
+			"Flex2": self._Flex2,
+			"Flex3": self._Flex3,
+			"Flex4": self._Flex4,
+			"FramerFlex1": self._FramerFlex1,
+			"FramerFlex2": self._FramerFlex2,
+			"Flex5": self._Flex5,
+			"Flex7": self._Flex7,
+			"TextBox1": self._TextBox1,
+			"TextBox2": self._TextBox2,
+			"Flex8": self._Flex8,
+			"Flex9": self._Flex9,
+			"Image1": self._Image1,
+			"Flex10": self._Flex10,
+			"TextBox3": self._TextBox3,
+			"TextBox4": self._TextBox4,
+			"Flex11": self._Flex11,
+			"TextBox5": self._TextBox5,
+			"Flex12": self._Flex12,
+			"Flex13": self._Flex13,
+			"TextBox6": self._TextBox6,
+			"Image2": self._Image2,
+			"Image3": self._Image3,
+			"TextBox7": self._TextBox7,
+			"Flex14": self._Flex14,
+			"Flex15": self._Flex15,
+			"Image4": self._Image4,
+			"TextBox8": self._TextBox8,
+			"Flex16": self._Flex16,
+			"Flex17": self._Flex17,
+			"Flex18": self._Flex18,
+			"Flex19": self._Flex19,
+			"Input2": self._Input2,
+			"Input3": self._Input3,
+			"Flex20": self._Flex20,
+			"Input4": self._Input4,
+			"Input5": self._Input5,
+			"Flex21": self._Flex21,
+			"Flex22": self._Flex22,
+			"Flex23": self._Flex23,
+			"Input6": self._Input6,
+			"Input7": self._Input7,
+			"Flex24": self._Flex24,
+			"Flex25": self._Flex25,
+			"Flex26": self._Flex26,
+			"Flex27": self._Flex27,
+			"Textarea1": self._Textarea1,
+			"ButtonFlex1": self._ButtonFlex1,
+			"TextBox9": self._TextBox9,
+			"Flex28": self._Flex28
 			}
   
